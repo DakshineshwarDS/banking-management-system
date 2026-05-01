10b5c64b265600895e691e18e5f272f69202965c
DakshineshwarDS <dakshineshwards@gmail.com>
2026-05-01 18:35:54 +0530

    Initial Banking Project setup

---
+++ src/main/java/com/bankapp/banking_management_system/entity/Account.java
@@ -0,0 +1,11 @@
+package com.bankapp.banking_management_system.entity;
+
+public class Account {
+
+    private Long id;
+    private String accountHolderName;
+    private String accountNumber;
+    private double balance;
+    private String accountType;
+
+}
